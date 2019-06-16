5ed6836375a7b692b4d63f14691d57976742f03c
yammik <mkim0514@gmail.com>
2019-06-16 06:02:19 -0400

    fix overlay

--- src/Landing.js
+++ src/Landing.js
@@ -4,18 +4,24 @@ import { text, wrappers, EvenSpace } from "./Styles";
 
 export default function Landing() {
   return (
-    <>
-      <wrappers.Blue>
+    <wrappers.Blue>
+      <Wrapper>
         <NameWrapper>
           <text.Heading>MAY</text.Heading>
           <text.Heading>KIM</text.Heading>
         </NameWrapper>
         <text.SubHeading>Software Engineer</text.SubHeading>
-        <wrappers.BlueOverlay />
-      </wrappers.Blue>
-    </>
+      </Wrapper>
+      <wrappers.BlueOverlay height={20} bottom={25} />
+      <wrappers.BlueOverlay height={35} bottom={35} />
+    </wrappers.Blue>
   );
 }
+
+const Wrapper = styled.div`
+  margin-left: 4rem;
+  margin-top: 6rem;
+`;
 
 const NameWrapper = styled(EvenSpace)`
   width: 9rem;
--- src/Styles.js
+++ src/Styles.js
@@ -2,17 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const colors = {
-  lBlue: "rgba(205, 218, 235, 0.3)",
-  dBlue: "rgba(153, 179, 227, 0.3)",
-  blueGreen: "rgba(129, 177, 156, 0.3)",
-  green: "rgba(222, 233, 228, 0.3)",
-  white: "#fff"
+  lBlue: "rgba(205, 218, 235, 0.6)",
+  lBlueFaded: "rgba(205, 218, 235, 0.3)",
+  dBlue: "rgba(153, 179, 227, 0.6)",
+  dBlueFaded: "rgba(153, 179, 227, 0.3)",
+  blueGreen: "rgba(129, 177, 156, 0.6)",
+  blueGreenFaded: "rgba(129, 177, 156, 0.3)",
+  green: "rgba(222, 233, 228, 0.6)",
+  greenFaded: "rgba(222, 233, 228, 0.3)",
+  white: "rgba(255,255,255, 0.6)",
+  whiteFaded: "rgba(255,255,255, 0.3)"
 };
 
 const ContentWrapper = styled.div`
   padding: 5rem;
   padding-bottom: 0;
-  background-color: ${({ color }) => color};
   z-index: -${({ order }) => order};
   position: relative;
   margin-bottom: 6rem;
@@ -21,26 +25,35 @@ const ContentWrapper = styled.div`
     z-index: -${({ order }) => order};
     content: "";
     position: absolute;
-    bottom: -10vw;
-    left: 0%;
+    top: 0;
+    bottom: 3rem;
+    left: 0;
     width: 0;
-    height: 0;
     border-right: 100vw solid transparent;
-    border-top: 10vw solid ${({ color }) => color};
   }
 `;
 
-const OverlayWrapper = styled(ContentWrapper)`
-  position: absolute;
+const OverlayWrapper = styled.div`
+  bottom: -3rem;
   left: 0;
+  position: absolute;
+  padding: 5rem;
+  padding-bottom: 0;
+  margin-bottom: 6rem;
   right: 0;
   top: 0;
-  z-index: -2;
-  padding: 5rem;
+  z-index: -${({ order }) => order};
 
   &:after {
-    bottom: -26vw;
-    border-top: 26vw solid ${({ color }) => color};
+    z-index: -${({ order }) => order};
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    bottom: -${({ bottom }) => bottom}vw;
+    border-left: 100vw solid ${({ color }) => color};
+    border-bottom: ${({ height }) => height}vw solid transparent;
   }
 `;
 
@@ -51,7 +64,7 @@ export const wrappers = {
     </ContentWrapper>
   ),
   BlueOverlay: props => (
-    <OverlayWrapper order={1} color={colors.lBlue}>
+    <OverlayWrapper {...props} order={1} color={colors.lBlueFaded}>
       {props.children}
     </OverlayWrapper>
   ),
@@ -61,7 +74,7 @@ export const wrappers = {
     </ContentWrapper>
   ),
   BluerOverlay: props => (
-    <OverlayWrapper order={2} color={colors.dBlue}>
+    <OverlayWrapper order={2} color={colors.dBlueFaded}>
       {props.children}
     </OverlayWrapper>
   ),
@@ -71,7 +84,7 @@ export const wrappers = {
     </ContentWrapper>
   ),
   BlueGreenOverlay: props => (
-    <OverlayWrapper order={3} color={colors.blueGreen}>
+    <OverlayWrapper order={3} color={colors.blueGreenFaded}>
       {props.children}
     </OverlayWrapper>
   ),
@@ -81,7 +94,7 @@ export const wrappers = {
     </ContentWrapper>
   ),
   GreenOverlay: props => (
-    <OverlayWrapper order={4} color={colors.green}>
+    <OverlayWrapper order={4} color={colors.greenFaded}>
       {props.children}
     </OverlayWrapper>
   ),
@@ -91,7 +104,7 @@ export const wrappers = {
     </ContentWrapper>
   ),
   WhiteOverlay: props => (
-    <OverlayWrapper order={5} color={colors.white}>
+    <OverlayWrapper order={5} color={colors.whiteFaded}>
       {props.children}
     </OverlayWrapper>
   )
